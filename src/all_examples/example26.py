bdbaf71f4e180bf0e5cf9a22a11dfeed7ef8a231
Egor Svetlichny <fkhor@inbox.ru>
2024-02-28 13:21:22 +0300

    Move the fetch_status function to the util package

--- src/all_examples/example26.py
+++ src/all_examples/example26.py
@@ -1,13 +1,7 @@
 import asyncio
 import aiohttp
-from aiohttp import ClientSession
 from src.util import async_timed
-
-
-@async_timed()
-async def fetch_status(session: ClientSession, url: str) -> int:
-    async with session.get(url) as result:
-        return result.status
+from src.util import fetch_status
 
 
 @async_timed()
--- src/util/__init__.py
+++ src/util/__init__.py
@@ -1,2 +1,3 @@
 from src.util.delay_functions import delay
 from src.util.async_timer import async_timed
+from src.util.chapter_04 import fetch_status
