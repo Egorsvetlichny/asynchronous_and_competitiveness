f0c1c557adf06e43242f0267ce4e7af1a989e270
Egor Svetlichny <fkhor@inbox.ru>
2024-02-28 13:35:08 +0300

    add example27. Set timeouts in aiohttp

---
+++ src/all_examples/example27.py
@@ -0,0 +1,22 @@
+# Если запрос к 'https://example.com' займёт больше 10мс, то поднимется исключение TimeoutError
+
+import asyncio
+import aiohttp
+from aiohttp import ClientSession
+
+
+async def fetch_status(session: ClientSession, url: str) -> int:
+    ten_millis = aiohttp.ClientTimeout(total=.01)
+
+    async with session.get(url, timeout=ten_millis) as result:
+        return result.status
+
+
+async def main():
+    session_timeout = aiohttp.ClientTimeout(total=1, connect=.1)
+
+    async with aiohttp.ClientSession(timeout=session_timeout) as session:
+        await fetch_status(session, 'https://example.com')
+
+
+asyncio.run(main())
