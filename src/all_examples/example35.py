63edfd0826a70f00187508c37bc449fbe014aeb1
Egor Svetlichny <fkhor@inbox.ru>
2024-03-01 13:52:08 +0300

    add example35. Processing requests as they are finalized

---
+++ src/all_examples/example35.py
@@ -0,0 +1,24 @@
+import asyncio
+import aiohttp
+from src.util import async_timed
+from src.util import fetch_status
+
+
+@async_timed()
+async def main():
+    async with aiohttp.ClientSession() as session:
+        url = 'https://www.example.com'
+        fetchers = [asyncio.create_task(fetch_status(session, url)),
+                    asyncio.create_task(fetch_status(session, url)),
+                    asyncio.create_task(fetch_status(session, url))]
+
+        done, pending = await asyncio.wait(fetchers, return_when=asyncio.FIRST_COMPLETED)
+
+        print(f'Число завершившихся задач: {len(done)}')
+        print(f'Число ожидающих задач: {len(pending)}')
+
+        for done_task in done:
+            print(await done_task)
+
+
+asyncio.run(main())
