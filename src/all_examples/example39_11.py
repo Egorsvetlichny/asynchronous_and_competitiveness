a807b6ae80840cd0e635fb22d5d8e029f6ec2a34
Egor Svetlichny <fkhor@inbox.ru>
2024-03-04 14:34:45 +0300

    add example39_11. Manual transaction management

---
+++ src/all_examples/example39_11.py
@@ -0,0 +1,31 @@
+import asyncio
+import asyncpg
+from asyncpg.transaction import Transaction
+
+
+async def main():
+    connection = await asyncpg.connect(host='127.0.0.1',
+                                       port=5432,
+                                       user='postgres',
+                                       database='products',
+                                       password='password')
+    transaction: Transaction = connection.transaction()
+    await transaction.start()
+    try:
+        await connection.execute("INSERT INTO brand VALUES(DEFAULT, 'brand_1')")
+        await connection.execute("INSERT INTO brand VALUES(DEFAULT, 'brand_2')")
+    except asyncpg.PostgresError:
+        print('Ошибка, транзакция откатывается!')
+        await transaction.rollback()
+    else:
+        print('Ошибки нет, транзакция фиксируется!')
+        await transaction.commit()
+
+    query = "SELECT brand_name FROM brand WHERE brand_name LIKE 'brand%'"
+    brands = await connection.fetch(query)
+    print(brands)
+
+    await connection.close()
+
+
+asyncio.run(main())
