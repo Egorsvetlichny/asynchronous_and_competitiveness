8b1ad857ed354a19f47add5d5b0088e46fee5a9b
Egor Svetlichny <fkhor@inbox.ru>
2024-03-04 17:57:19 +0300

    add product query to bd_commands

--- src/all_examples/example39_6.py
+++ src/all_examples/example39_6.py
@@ -1,20 +1,6 @@
 import asyncio
 import asyncpg
-
-product_query = \
-    """
-    SELECT
-    p.product_id,
-    p.product_name,
-    p.brand_id,
-    s.sku_id,
-    pc.product_color_name,
-    ps.product_size_name
-    FROM product as p
-    JOIN sku as s on s.product_id = p.product_id
-    JOIN product_color as pc on pc.product_color_id = s.product_color_id
-    JOIN product_size as ps on ps.product_size_id = s.product_size_id
-    WHERE p.product_id = 100"""
+from src.bd_commands import product_query
 
 
 async def query_product(pool):
--- src/bd_commands/__init__.py
+++ src/bd_commands/__init__.py
@@ -1,1 +1,2 @@
 from src.bd_commands.create_base_tables import *
+from src.bd_commands.product_query import product_query
