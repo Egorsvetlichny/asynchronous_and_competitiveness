e234142bde8de85d4b505aa455c3908441e2528d
Egor Svetlichny <fkhor@inbox.ru>
2024-03-04 18:04:49 +0300

    optimize database query definition

--- src/all_examples/example 39_7.py
+++ src/all_examples/example 39_7.py
@@ -1,21 +1,7 @@
 import asyncio
 import asyncpg
 from src.util import async_timed
-
-product_query = \
-    """
-    SELECT
-    p.product_id,
-    p.product_name,
-    p.brand_id,
-    s.sku_id,
-    pc.product_color_name,
-    ps.product_size_name
-    FROM product as p
-    JOIN sku as s on s.product_id = p.product_id
-    JOIN product_color as pc on pc.product_color_id = s.product_color_id
-    JOIN product_size as ps on ps.product_size_id = s.product_size_id
-    WHERE p.product_id = 100"""
+from src.bd_commands import product_query
 
 
 async def query_product(pool):
