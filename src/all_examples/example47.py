1f6768ae2c902ab0a99700727454686b110910ae
Egor Svetlichny <fkhor@inbox.ru>
2024-03-06 17:42:54 +0300

    add example47. Parallelization of reduce operation

--- src/all_examples/example47.py
+++ src/all_examples/example47.py
@@ -3,34 +3,23 @@ import concurrent.futures
 import functools
 import time
 from typing import Dict, List
+from src.all_examples.example46 import partition, merge_dictionaries, map_frequencies
 
 
-def partition(data: List, chunk_size: int) -> List:
-    for i in range(0, len(data), chunk_size):
-        yield data[i:i + chunk_size]
+async def reduce(loop, pool, counters, chunk_size) -> Dict[str, int]:
+    chunks: List[List[Dict]] = list(partition(counters, chunk_size))
+    reducers = []
 
+    while len(chunks[0]) > 1:
+        for chunk in chunks:
+            reducer = functools.partial(functools.reduce, merge_dictionaries, chunk)
+            reducers.append(loop.run_in_executor(pool, reducer))
 
-def map_frequencies(chunk: List[str]) -> Dict[str, int]:
-    counter = {}
-    for line in chunk:
-        word, _, count, _ = line.split('\t')
-        if counter.get(word):
-            counter[word] = counter[word] + int(count)
-        else:
-            counter[word] = int(count)
+        reducer_chunks = await asyncio.gather(*reducers)
+        chunks = list(partition(reducer_chunks, chunk_size))
+        reducers.clear()
 
-    return counter
-
-
-def merge_dictionaries(first: Dict[str, int], second: Dict[str, int]) -> Dict[str, int]:
-    merged = first
-    for key in second:
-        if key in merged:
-            merged[key] = merged[key] + second[key]
-        else:
-            merged[key] = second[key]
-
-    return
+    return chunks[0][0]
 
 
 async def main(partition_size: int):
@@ -40,14 +29,15 @@ async def main(partition_size: int):
         contents = f.readlines()
         loop = asyncio.get_running_loop()
         tasks = []
-        start = time.time()
 
         with concurrent.futures.ProcessPoolExecutor() as pool:
+            start = time.time()
+
             for chunk in partition(contents, partition_size):
                 tasks.append(loop.run_in_executor(pool, functools.partial(map_frequencies, chunk)))
 
             intermediate_results = await asyncio.gather(*tasks)
-            final_result = functools.reduce(merge_dictionaries, intermediate_results)
+            final_result = await reduce(loop, pool, intermediate_results, 500)
 
             print(f"anteriori встречается {final_result['anteriori']} раз.")
 
@@ -56,4 +46,4 @@ async def main(partition_size: int):
 
 
 if __name__ == "__main__":
-    asyncio.run(main(partition_size=60000))
+    asyncio.run(main(partition_size=5000))
